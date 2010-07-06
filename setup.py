f8da6b04a36bb2f99423f0f1e43ac7919bc6cd62
Thane Thomson <thane@praekelt.com>
2010-07-06 21:25:46 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Django Form Designer
+
+Original code copyright (c) 2009, Samuel Luescher
+setup.py added by Thane Thomson (thane@praekelt.com), Praekelt Digital
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-form-designer',
+    version='0.1',
+    description='Construct Django forms through the Django admin.',
+    author='Praekelt Digital',
+    author_email='dev@praekelt.com',
+    url='https://github.com/thanethomson/django-form-designer',
+    packages = find_packages(),
+    include_package_data=True,
+)
